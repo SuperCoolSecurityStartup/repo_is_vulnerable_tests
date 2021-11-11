8552e05a19a001ce53e4473c0a4c91bfec58368d
omarnbou <45265515+omarnbou@users.noreply.github.com>
2021-11-11 16:15:53 -0500

    Create csharp_test.cs

---
+++ csharp_test.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace WebAppSSLManager.Models
+{
+    public class Constants
+    {
+        public const string AppPropertiesFileName = "appproperties.json";
+        public const string CertInfoFileName = "certinfo.json";
+        public const string AccountKeyFileName = "accountkey.pem";
+        public const string DefaultCertificatePassword = "C0mpl1c4t3d57r1ng";
+    }
+}
