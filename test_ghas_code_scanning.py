73fcf51718e74b8cfc523944b9b318ca932e1908
omarnbou <45265515+omarnbou@users.noreply.github.com>
2021-11-11 13:01:01 -0500

    Update test_ghas_code_scanning.py

--- test_ghas_code_scanning.py
+++ test_ghas_code_scanning.py
@@ -5,3 +5,8 @@ import requests
 api_token = "Password_testing_github_code_scanning"
 
 print(api_token)
+
+import subprocess
+domain = input("Enter the Domain: ")
+output = subprocess.check_output(f"nslookup {domain}", shell=True, encoding='UTF-8')
+print(output)
